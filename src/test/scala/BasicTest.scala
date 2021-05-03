572350d368950eb05ec6c6d0825f23cdb45e5422
LinJiawei <linjiawei20s@ict.ac.cn>
2021-05-03 22:54:15 +0800

    add abstract tester for l2

---
+++ src/test/scala/BasicTest.scala
@@ -0,0 +1,9 @@
+package HuanCun
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class DummyTest {
+
+}
---
+++ src/test/scala/L2Tester.scala
@@ -0,0 +1,9 @@
+package HuanCun
+
+import chiseltest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should._
+
+abstract class L2Tester extends AnyFlatSpec with ChiselScalatestTester with Matchers {
+  behavior of "L2"
+}
