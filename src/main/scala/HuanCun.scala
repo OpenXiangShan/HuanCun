70d141a8183ffb78781159121a1943bf3ad7bfc3
wangkaifan <wangkaifan@ict.ac.cn>
2021-05-03 22:15:06 +0800

    huancun: setup naive top framework

--- src/main/scala/HuanCun.scala
+++ src/main/scala/HuanCun.scala
@@ -1,4 +1,69 @@
 package HuanCun
 
-class HuanCun {
+import chipsalliance.rocketchip.config.Parameters
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink._
+
+class HuanCun(val cache: CacheParameters)(implicit p: Parameters)
+    extends LazyModule {
+
+  val xfer = TransferSizes(cache.blockBytes, cache.blockBytes)
+  val atom = TransferSizes(1, cache.beatBytes)
+  val access = TransferSizes(1, cache.blockBytes)
+
+  val node: TLAdapterNode = TLAdapterNode(
+    clientFn = { _ =>
+      TLClientPortParameters(
+        Seq(
+          TLClientParameters(
+            name = "L2 HuanCun",
+            supportsProbe = xfer
+          )
+        )
+      )
+    },
+    managerFn = { m =>
+      TLManagerPortParameters(
+        managers = m.managers.map { m =>
+          m.copy(
+            regionType =
+              if (m.regionType >= RegionType.UNCACHED) RegionType.CACHED
+              else m.regionType,
+            supportsAcquireB = xfer,
+            supportsAcquireT =
+              if (m.supportsAcquireT) xfer else TransferSizes.none,
+            supportsArithmetic =
+              if (m.supportsAcquireT) atom else TransferSizes.none,
+            supportsLogical =
+              if (m.supportsAcquireT) atom else TransferSizes.none,
+            supportsGet = access,
+            supportsPutFull =
+              if (m.supportsAcquireT) access else TransferSizes.none,
+            supportsPutPartial =
+              if (m.supportsAcquireT) access else TransferSizes.none,
+            supportsHint = access,
+            alwaysGrantsT = false,
+            fifoId = None
+          )
+        },
+        beatBytes = cache.beatBytes,
+        minLatency = 2
+      )
+    }
+  )
+
+  lazy val module = new LazyModuleImp(this) {
+
+    require(node.in.size == node.out.size)
+
+    (node.in zip node.out) foreach { case ((in, edgeIn), (out, edgeOut)) =>
+      out.a.bits := in.a.bits
+      in.d.bits := out.d.bits
+      in.a.ready := out.a.ready
+      out.a.valid := in.a.valid
+      in.d.valid := out.d.valid
+      out.d.ready := in.d.ready
+    }
+
+  }
 }
---
+++ src/main/scala/Parameters.scala
@@ -0,0 +1,29 @@
+package HuanCun
+
+import chisel3._
+import chisel3.util._
+
+case class CacheParameters(
+    // TODO: remove the following fixed parameters
+    level: Int = 2,
+    ways: Int = 4,
+    sets: Int,
+    blockBytes: Int = 64,
+    beatBytes: Int = 32,
+    replacement: String = "plru",
+    uncachedGet: Boolean = false,
+    debug: Boolean = false,
+    enablePerf: Boolean = false,
+    verification: Boolean = false
+) // inner
+{
+  require(ways > 0)
+  require(sets > 0)
+  require(blockBytes > 0 && isPow2(blockBytes))
+  require(beatBytes > 0 && isPow2(beatBytes))
+  require(blockBytes >= beatBytes)
+
+  val blocks = ways * sets
+  val sizeBytes = blocks * blockBytes
+  val blockBeats = blockBytes / beatBytes
+}
