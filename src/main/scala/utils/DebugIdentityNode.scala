1f46ecd7b7c6e0a993c260a1ad27a8261af7bed5
wangkaifan <wangkaifan@ict.ac.cn>
2021-05-04 18:23:52 +0800

    util: add some necessary utils

---
+++ src/main/scala/utils/DebugIdentityNode.scala
@@ -0,0 +1,52 @@
+package HuanCun.utils
+
+import chisel3._
+import chipsalliance.rocketchip.config.Parameters
+import chisel3.util.DecoupledIO
+import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp}
+import freechips.rocketchip.tilelink.{
+  TLBundle,
+  TLClientNode,
+  TLIdentityNode,
+  TLMasterParameters,
+  TLMasterPortParameters
+}
+
+class DebugIdentityNode()(implicit p: Parameters) extends LazyModule {
+
+  val node = TLIdentityNode()
+
+  val n = TLClientNode(
+    Seq(
+      TLMasterPortParameters.v1(
+        Seq(
+          TLMasterParameters.v1("debug node")
+        )
+      )
+    )
+  )
+
+  lazy val module = new LazyModuleImp(this) {
+    val (out, _) = node.out(0)
+    val (in, _) = node.in(0)
+
+    def debug(t: TLBundle, valid: Boolean = false): Unit = {
+      def fire[T <: Data](x: DecoupledIO[T]) = if (valid) x.valid else x.fire()
+      val channels = Seq(t.a, t.b, t.c, t.d, t.e)
+      channels.foreach(c =>
+        when(fire(c)) {
+          printf(" isFire:%d [TODO: fix me]", c.fire())
+//          c.bits.dump
+        }
+      )
+    }
+    debug(in, false)
+  }
+}
+
+object DebugIdentityNode {
+  def apply()(implicit p: Parameters): TLIdentityNode = {
+    val identityNode = LazyModule(new DebugIdentityNode())
+    identityNode.node
+  }
+}
