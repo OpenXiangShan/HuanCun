a68830b755583ff74f14ffd09b56769c6e388020
LinJiawei <linjiawei20s@ict.ac.cn>
2021-05-23 10:41:19 +0800

    simplify sourceId comparison

--- src/main/scala/huancun/Common.scala
+++ src/main/scala/huancun/Common.scala
@@ -3,18 +3,15 @@ package huancun
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 
-class SinkAReq(implicit p: Parameters) extends HuanCunBundle {
+abstract class InnerTask(implicit p: Parameters) extends HuanCunBundle {
   val sourceId = UInt(sourceIdBits.W)
 }
-class SourceBReq(implicit p: Parameters) extends HuanCunBundle {
-  val sourceId = UInt(sourceIdBits.W)
-}
-class SinkCReq(implicit p: Parameters) extends HuanCunBundle {
-  val sourceId = UInt(sourceIdBits.W)
-}
-class SourceDReq(implicit p: Parameters) extends HuanCunBundle {
-  val sourceId = UInt(sourceIdBits.W)
-}
+
+class SinkAReq(implicit p: Parameters) extends InnerTask
+class SourceBReq(implicit p: Parameters) extends InnerTask
+class SinkCReq(implicit p: Parameters) extends InnerTask
+class SourceDReq(implicit p: Parameters) extends InnerTask
+
 class SourceAReq(implicit p: Parameters) extends HuanCunBundle
 class SourceCReq(implicit p: Parameters) extends HuanCunBundle
 class SourceEReq(implicit p: Parameters) extends HuanCunBundle
