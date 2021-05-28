fb0a3d11ff3e1575ca76e15453e24067e720b082
wangkaifan <wangkaifan@ict.ac.cn>
2021-05-28 20:40:15 +0800

    sinkA: add simple logic for sinkA

--- src/main/scala/huancun/SinkA.scala
+++ src/main/scala/huancun/SinkA.scala
@@ -11,4 +11,32 @@ class SinkA(val edge: TLEdgeIn)(implicit p: Parameters) extends HuanCunModule {
     val alloc = DecoupledIO(new MSHRRequest)
     val task = Flipped(DecoupledIO(new SinkAReq))
   })
+
+  // TODO: Handle task
+  io.task.ready := false.B
+
+  def parseAddress(x: UInt): (UInt, UInt, UInt) = {
+    val offset = x  // TODO: check address mapping
+    val set = offset >> offsetBits
+    val tag = set >> setBits
+    (tag(tagBits-1, 0), set(setBits-1, 0), offset(offsetBits-1, 0))
+  }
+
+  val a = io.a
+  val first = edge.first(a)
+  val hasData = edge.hasData(a.bits)
+  assert(!hasData)
+  val (tag, set, offset) = parseAddress(a.bits.address)
+
+  io.alloc.valid := a.valid && first
+
+  val allocInfo = io.alloc.bits
+  allocInfo.channel := 1.U(3.W)
+  allocInfo.opcode := a.bits.opcode
+  allocInfo.param := a.bits.param
+  allocInfo.size := a.bits.size
+  allocInfo.source := a.bits.source
+  allocInfo.set := set
+  allocInfo.tag := tag
+  allocInfo.off := offset
 }
