6f45041e20dae81c7b6427db6a810533d12e2487
LinJiawei <linjiawei20s@ict.ac.cn>
2021-05-28 20:34:53 +0800

    Impl SourceA

--- src/main/scala/huancun/SourceA.scala
+++ src/main/scala/huancun/SourceA.scala
@@ -9,4 +9,18 @@ class SourceA(edge: TLEdgeOut)(implicit p: Parameters) extends HuanCunModule {
     val a = DecoupledIO(new TLBundleA(edge.bundle))
     val task = Flipped(DecoupledIO(new SourceAReq))
   })
+
+  val a = io.a
+
+  io.task.ready := a.ready
+  a.valid := io.task.valid
+
+  a.bits.opcode := io.task.bits.opcode
+  a.bits.param := io.task.bits.param
+  a.bits.size := offsetBits.U
+  a.bits.source := io.task.bits.source
+  a.bits.address :=  Cat(io.task.bits.tag, io.task.bits.set, 0.U(offsetBits.W))
+  a.bits.mask := Fill(edgeOut.manager.beatBytes, 1.U(1.W))
+  a.bits.data := DontCare
+
 }
