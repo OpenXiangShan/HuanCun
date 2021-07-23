6a21977fd0b099b0b4009ac1ae633041708301f6
wangkaifan <wangkaifan@ict.ac.cn>
2021-07-23 19:30:02 +0800

    test: fix broken multi-acquire test

--- src/test/scala/huancun/tlctest/MultiAcquireTester.scala
+++ src/test/scala/huancun/tlctest/MultiAcquireTester.scala
@@ -6,16 +6,18 @@ import huancun.DumpVCD
 import tltest.TLMessagesBigInt
 
 class MultiAcquireTester extends TLCTest with DumpVCD {
-  test(testTop.module).withAnnotations(testAnnos){ dut =>
-    for (i <- 0 until 5) {
-      testTop.l1d.agent.addAcquire(512*i, TLMessagesBigInt.toT)
-    }
-    while (testTop.l1d.agent.outerAcquire.nonEmpty) {
-      testTop.l1d.agent.issueA()
-      testTop.l1d.agent.issueC()
-      testTop.l1d.update(dut.io)
-      testTop.l1d.agent.step()
-      dut.clock.step(1)
+  it should "do multiple acquire" in {
+    test(testTop.module).withAnnotations(testAnnos) { dut =>
+      for (i <- 0 until 5) {
+        testTop.l1d.agent.addAcquire(512 * i, TLMessagesBigInt.toT)
+      }
+      while (testTop.l1d.agent.outerAcquire.nonEmpty) {
+        testTop.l1d.agent.issueA()
+        testTop.l1d.agent.issueC()
+        testTop.l1d.update(dut.io)
+        testTop.l1d.agent.step()
+        dut.clock.step(1)
+      }
     }
   }
 }
