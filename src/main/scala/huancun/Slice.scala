97f26cf0d6ce82701398f037e058290eb78c9123
Lemover <zhangzifei20z@ict.ac.cn>
2021-07-31 09:31:12 +0800

    add Mulan PSL2 license (#15)
    * add license
    
    * code clean up
    
    Co-authored-by: LinJiawei <linjiawei20s@ict.ac.cn>

--- src/main/scala/huancun/Slice.scala
+++ src/main/scala/huancun/Slice.scala
@@ -1,3 +1,22 @@
+/** *************************************************************************************
+  * Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+  * Copyright (c) 2020-2021 Peng Cheng Laboratory
+  *
+  * XiangShan is licensed under Mulan PSL v2.
+  * You can use this software according to the terms and conditions of the Mulan PSL v2.
+  * You may obtain a copy of Mulan PSL v2 at:
+  *          http://license.coscl.org.cn/MulanPSL2
+  *
+  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+  * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+  *
+  * See the Mulan PSL v2 for more details.
+  * *************************************************************************************
+  */
+
+// See LICENSE.SiFive for license details.
+
 package huancun
 
 import chipsalliance.rocketchip.config.Parameters
@@ -151,8 +170,9 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
   }
 
   // Provide MSHR info for sinkC, sinkD
-  sinkC.io.way := Mux(bc_mshr.io.status.valid &&
-    bc_mshr.io.status.bits.set === sinkC.io.resp.bits.set,
+  sinkC.io.way := Mux(
+    bc_mshr.io.status.valid &&
+      bc_mshr.io.status.bits.set === sinkC.io.resp.bits.set,
     bc_mshr.io.status.bits.way,
     Mux1H(
       abc_mshr.map(m => m.io.status.valid && m.io.status.bits.set === sinkC.io.resp.bits.set),
