1bbef89b69b3bdd1d7afefad2382a8003be0d231
LinJiawei <linjiawei20s@ict.ac.cn>
2021-08-24 17:47:43 +0800

    Inclusive cache and non-inclusive cache can share SubDirectory

--- src/main/scala/huancun/BaseDirectory.scala
+++ src/main/scala/huancun/BaseDirectory.scala
@@ -22,6 +22,8 @@ package huancun
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 import chisel3.util._
+import chisel3.util.random.LFSR
+import huancun.utils.{ParallelPriorityMux, SRAMTemplate}
 
 trait BaseDirResult extends HuanCunBundle {
   val idOH = UInt(mshrsAll.W) // which mshr the result should be sent to
@@ -52,4 +54,117 @@ abstract class BaseDirectory[T_RESULT <: BaseDirResult, T_DIR_W <: BaseDirWrite,
   implicit p: Parameters)
     extends HuanCunModule {
   val io: BaseDirectoryIO[T_RESULT, T_DIR_W, T_TAG_W]
+}
+
+class SubDirectory[T <: Data](
+  rports:      Int,
+  wports:      Int,
+  sets:        Int,
+  ways:        Int,
+  tagBits:     Int,
+  dir_init_fn: () => T,
+  dir_hit_fn:  T => Bool)
+    extends Module {
+
+  val setBits = log2Ceil(sets)
+  val wayBits = log2Ceil(ways)
+  val dir_init = dir_init_fn()
+
+  val io = IO(new Bundle() {
+    val reads = Vec(
+      rports,
+      Flipped(DecoupledIO(new Bundle() {
+        val tag = UInt(tagBits.W)
+        val set = UInt(setBits.W)
+      }))
+    )
+    val resps = Vec(
+      rports,
+      ValidIO(new Bundle() {
+        val hit = Bool()
+        val way = UInt(wayBits.W)
+        val tag = UInt(tagBits.W)
+        val dir = dir_init.cloneType
+      })
+    )
+    val tag_w = Flipped(DecoupledIO(new Bundle() {
+      val tag = UInt(tagBits.W)
+      val set = UInt(setBits.W)
+      val way = UInt(wayBits.W)
+    }))
+    val dir_w = Vec(
+      wports,
+      Flipped(DecoupledIO(new Bundle() {
+        val set = UInt(setBits.W)
+        val way = UInt(wayBits.W)
+        val dir = dir_init.cloneType
+      }))
+    )
+  })
+
+  val resetFinish = RegInit(false.B)
+  val resetIdx = RegInit((sets - 1).U)
+  val metaArray = Mem(sets, Vec(ways, dir_init.cloneType))
+
+  when(!resetFinish) {
+    metaArray(resetIdx).foreach(w => w := dir_init)
+    resetIdx := resetIdx - 1.U
+  }
+  when(resetIdx === 0.U) {
+    resetFinish := true.B
+  }
+
+  io.tag_w.ready := true.B
+  io.reads.foreach(_.ready := !io.tag_w.valid && resetFinish)
+
+  val tagArray = Array.fill(rports) {
+    Module(new SRAMTemplate(UInt(tagBits.W), sets, ways, singlePort = true))
+  }
+
+  val tagRead = Seq.fill(rports) {
+    Wire(Vec(ways, UInt(tagBits.W)))
+  }
+  tagArray
+    .zip(io.reads)
+    .zip(tagRead)
+    .foreach({
+      case ((sram, rreq), rdata) =>
+        val wen = io.tag_w.fire()
+        sram.io.w(wen, io.tag_w.bits.tag, io.tag_w.bits.set, UIntToOH(io.tag_w.bits.way))
+        assert(!wen || (wen && sram.io.w.req.ready))
+        rdata := sram.io.r(!wen, rreq.bits.set).resp.data
+        assert(wen || (!wen && sram.io.r.req.ready))
+    })
+
+  val reqTags = io.reads.map(r => RegEnable(r.bits.tag, enable = r.fire()))
+  val reqSets = io.reads.map(r => RegEnable(r.bits.set, enable = r.fire()))
+  val reqValids = io.reads.map(r => RegNext(r.fire(), false.B))
+
+  val hitVec = Seq.fill(rports)(Wire(Vec(ways, Bool())))
+  for ((result, i) <- io.resps.zipWithIndex) {
+    result.valid := reqValids(i)
+    val tags = tagRead(i)
+    val metas = metaArray(reqSets(i))
+    val tagMatchVec = tags.map(_ === reqTags(i))
+    val metaValidVec = metas.map(dir_hit_fn)
+    hitVec(i) := tagMatchVec.zip(metaValidVec).map(a => a._1 && a._2)
+    val hitWay = OHToUInt(hitVec(i))
+    val replaceWay = LFSR(wayBits) // TODO: improve this
+    val invalidWay = ParallelPriorityMux(metaValidVec.zipWithIndex.map(a => (!a._1, a._2.U)))
+    val chosenWay = Mux(Cat(metaValidVec).andR(), replaceWay, invalidWay)
+
+    result.bits.hit := Cat(hitVec(i)).orR()
+    result.bits.way := Mux(result.bits.hit, hitWay, chosenWay)
+    val meta = metas(result.bits.way)
+    result.bits.dir := meta
+    result.bits.tag := tags(result.bits.way)
+  }
+
+  for (req <- io.dir_w) {
+    when(req.fire()) {
+      metaArray(req.bits.set)(req.bits.way) := req.bits.dir
+    }
+    req.ready := true.B
+  }
+
 }
