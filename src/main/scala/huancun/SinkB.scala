87d7c90f9bd543cc2ada5eb64ad981a66187ce4b
LinJiawei <linjiawei20s@ict.ac.cn>
2021-08-24 17:28:34 +0800

    misc: remove dontcare in SourceB/SinkB

--- src/main/scala/huancun/SinkB.scala
+++ src/main/scala/huancun/SinkB.scala
@@ -24,7 +24,7 @@ import chisel3._
 import chisel3.util._
 import freechips.rocketchip.tilelink._
 
-class SinkB(edge: TLEdgeOut)(implicit p: Parameters) extends HuanCunModule with DontCareInnerLogic {
+class SinkB(edge: TLEdgeOut)(implicit p: Parameters) extends HuanCunModule {
   val io = IO(new Bundle() {
     val b = Flipped(DecoupledIO(new TLBundleB(edge.bundle)))
     val alloc = DecoupledIO(new MSHRRequest)
