9afb08ceafa8c2f141c4915138d13f487a10850c
LinJiawei <linjiawei20s@ict.ac.cn>
2021-09-07 12:54:34 +0800

    Rewrite state_transition.py

--- scripts/state_transition.py
+++ scripts/state_transition.py
@@ -1,84 +1,130 @@
-# define some consts
-Invalid = 0
-Trunk = 1
-Branch = 2
-Tip = 3
+import itertools
+from enum import Enum, unique
 
-Dirty = 0
-Clean = 1
+@unique
+class Block(Enum):
+  NULL = 0
+  F = 1
+  G = 2
+  H = 3
+  I = 4
 
-stateDumpMap = dict([(Invalid, 'Invalid'), (Trunk, 'Trunk'), (Branch, 'Branch'), (Tip, 'Tip')])
+@unique
+class TLState(Enum):
+  INVALID = 0
+  BRANCH = 1
+  TRUNK = 2
+  TIP = 3
 
-class SelfDir:
-    
-    def __init__(self):
-        pass
+@unique
+class DirtyState(Enum):
+  CLEAN = 0
+  DIRTY = 1
 
-    def deserialize(self, num):
-        self.clientStates = num // 64
-        self.dirty = (num % 64) // 32
-        self.block = (num % 32) // 8
-        self.state = (num % 8) // 2
-        self.hit = (num % 2)
-        return self
-
-    def serialize(self):
-        return self.hit + self.state * 2 + self.block * 8 + self.dirty * 32 + self.clientStates * 64
-
-    def dump(self):
-        print(stateDumpMap[self.state])
-
-    nrPossible = 256
-
+@unique
+class HitState(Enum):
+  MISS = 0
+  HIT = 1
 
 class ClientDir:
-    def __init__(self):
-        pass
+  def __init__(self, tl_state):
+    self.tl_state = tl_state
+  def __str__(self):
+    return (f"{self.tl_state}")
 
-    def deserialize(self, num):
-        self.block = num // 8
-        self.state = (num % 8) // 2
-        self.hit = (num % 2)
-        return self
+class SelfDir:
+  def __init__(self, tl_state, dirty_state, hit_state, client_tl_states):
+    self.tl_state = tl_state
+    self.dirty_state = dirty_state
+    self.hit_state = hit_state
+    self.client_tl_states = client_tl_states
+  def __str__(self):
+    client_str = ""
+    for i, s in enumerate(self.client_tl_states):
+      client_str += f"Client {i}: {s}\n"
+    return (f"{client_str}Self: {self.tl_state} {self.dirty_state} {self.hit_state}")
 
-    def serialize(self):
-        return self.hit + self.state * 2 + self.block * 8
+class BlockState:
 
-    def dump(self):
-        print(stateDumpMap[self.state])
+  def __init__(self, req_block, self_block, client_blocks):
+    self.req_block = req_block
+    self.self_block = self_block
+    self.client_blocks = client_blocks
 
-    nrPossible = 32
+  def __str__(self):
+    req_str = f"req: {self.req_block}\n"
+    client_str = ""
+    for i, b in enumerate(self.client_blocks):
+      client_str += f"Client {i}: {b}\n"
+    self_str = f"Self: {self.self_block}\n"
+    return req_str + client_str + self_str
 
+class DirState:
+  def __init__(self, self_dir, client_dirs, block_state):
+    self.self_dir = self_dir
+    self.client_dirs = client_dirs
+    self.block_state = block_state
+  def __str__(self):
+    delim = "---------------------------------------------------\n"
+    client_str = "Client Dir:\n"
+    for i, c in enumerate(self.client_dirs):
+      client_str += f"Client {i}: {c}\n"
+    self_str = "Self Dir:\n" + str(self.self_dir) + "\n"
+    block_str = f"BlockState:\n{self.block_state}"
+    return delim + client_str + self_str + block_str + delim
 
-class DirStatus:
-    def __init__(self):
-        pass
+def get_all_block_states():
+  block_states = []
+  blocks = list(Block)[1:] # NULL block is special
+  def dfs(idx, acc):
+    if idx == len(blocks):
+      req_block, self_block = acc[:2]
+      client_blocks = acc[2:]
+      s = BlockState(req_block, self_block, client_blocks)
+      block_states.append(s)
+      return
+    for i in range(0, idx+1):
+      acc_next = acc.copy()
+      acc_next.append(blocks[i])
+      dfs(idx+1, acc_next)
+    if i != 0:
+      acc_next = acc.copy()
+      acc_next.append(Block.NULL)
+      dfs(idx+1, acc_next)
+  dfs(0, [])
+  # for s in block_states:
+  #   print(s)
+  # print(len(block_states))
+  return block_states
 
-    def deserialize(self, num):
-        selfDir = SelfDir()
-        clientDir = ClientDir()
-        self.selfDir = selfDir.deserialize(num // ClientDir.nrPossible)
-        self.clientDirs = clientDir.deserialize(num % ClientDir.nrPossible)
-        return self
+NUM_CLIENTS = 2
 
-    def isValid(self):
-        return True
+self_fields = [
+  list(TLState),
+  list(DirtyState),
+  list(HitState),
+  get_all_block_states()
+]
 
-    def dump(self):
-        print('=============')
-        self.selfDir.dump()
-        self.clientDir.dump()
+client_fields = []
 
-    nrPossible = ClientDir.nrPossible * SelfDir.nrPossible
+for i in range(0, NUM_CLIENTS):
+  self_fields.append(list(TLState))
+  client_fields.append(list(TLState))
 
+def get_all_states():
+  all_states = []
+  for x in itertools.product(*self_fields):
+    self_state, dirty, hit, block_state = x[:4]
+    self_clients = x[4:]
+    self_dir = SelfDir(self_state, dirty, hit, self_clients)
+    for c in itertools.product(*client_fields):
+      client_dirs = [ ClientDir(c[i]) for i in range(len(c)) ]
+      new_state = DirState(self_dir, client_dirs, block_state)
+      print(new_state)
+      all_states.append(new_state)
+  print(f"states: {len(all_states)}")
+  return all_states
 
-if __name__ == '__main__':
-    dirStatusList = []
-    for i in range(DirStatus.nrPossible):
-        dirStatus = DirStatus()
-        if dirStatus.deserialize(i).isValid():
-            dirStatusList.append(dirStatus.deserialize(i))
-
-    for item in dirStatusList:
-        item.dump()
+get_all_states()
 
