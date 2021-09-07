d9fbdb7b6bde01161f64c6cc4fa81ea4cbfd591d
LinJiawei <linjiawei20s@ict.ac.cn>
2021-09-07 15:30:40 +0800

    state_transition.py: retab

--- scripts/state_transition.py
+++ scripts/state_transition.py
@@ -157,10 +157,10 @@ def hit_filter(s):
   return True
 
 def retrieve_name(var):
-	for fi in reversed(inspect.stack()):
-		names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
-		if len(names) > 0:
-			return names[0]
+  for fi in reversed(inspect.stack()):
+    names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
+    if len(names) > 0:
+      return names[0]
 
 filters = [
   invalid_filter,
