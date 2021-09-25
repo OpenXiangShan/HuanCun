5cf7ddecf0674c0f4151d220702359a5b1158ab5
LinJiawei <linjiawei20s@ict.ac.cn>
2021-09-25 14:40:24 +0800

    request buffer: also put prefetch req into buffer

--- src/main/scala/huancun/RequestBuffer.scala
+++ src/main/scala/huancun/RequestBuffer.scala
@@ -3,26 +3,29 @@ package huancun
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 import chisel3.util._
-import freechips.rocketchip.tilelink.TLBundleA
+import huancun.utils.XSPerfAccumulate
 
 class RequestBuffer(flow: Boolean = true, entries: Int = 16)(implicit p: Parameters) extends HuanCunModule {
 
-  def bundle = new TLBundleA(edgeIn.bundle)
-
   val io = IO(new Bundle() {
-    val in = Flipped(DecoupledIO(bundle))
-    val out = DecoupledIO(bundle)
+    val in = Flipped(DecoupledIO(new MSHRRequest))
+    val out = DecoupledIO(new MSHRRequest)
     val mshr_status = Vec(mshrs, Flipped(ValidIO(new MSHRStatus)))
-    val mshr_alloc = Vec(mshrs, Flipped(ValidIO(new MSHRRequest)))
   })
 
-  val buffer = Mem(entries, bundle)
+  val buffer = Mem(entries, new MSHRRequest)
   val valids = RegInit(VecInit(Seq.fill(entries){ false.B }))
   // which mshr the entry is waiting for
   val wait_table = Reg(Vec(entries, UInt(mshrs.W)))
+  /*
+      buffer_dep_mask[i][j] => entry i should wait entry j
+      this is used to make sure that same set requests will be sent
+      to MSHR in order
+   */
+  val buffer_dep_mask = Reg(Vec(entries, Vec(entries, Bool())))
   val rdys = RegInit(VecInit(Seq.fill(entries){ false.B }))
 
-  val issue_arb = Module(new RRArbiter(bundle, entries))
+  val issue_arb = Module(new RRArbiter(new MSHRRequest, entries))
   for (i <- 0 until entries){
     issue_arb.io.in(i).valid := valids(i) && rdys(i)
     issue_arb.io.in(i).bits := buffer(i)
@@ -32,41 +35,65 @@ class RequestBuffer(flow: Boolean = true, entries: Int = 16)(implicit p: Paramet
   }
 
   val full = Cat(valids).andR()
-  val empty = !Cat(valids).orR()
-  io.out.bits := Mux(empty && flow.B, io.in.bits, issue_arb.io.out.bits)
-  io.out.valid := (flow.B && empty && io.in.valid) | issue_arb.io.out.valid
+  val no_ready_entry = !Cat(issue_arb.io.in.map(_.valid)).orR()
+  io.out.bits := Mux(no_ready_entry && flow.B, io.in.bits, issue_arb.io.out.bits)
+  io.out.valid := (flow.B && no_ready_entry && io.in.valid) | issue_arb.io.out.valid
   issue_arb.io.out.ready := io.out.ready
 
   io.in.ready := !full
 
-  val (_, input_set, _) = parseAddress(io.in.bits.address)
+  val in_set = io.in.bits.set
 
   def set_conflict(set_a: UInt, set_b: UInt): Bool = {
     set_a(block_granularity - 1, 0) === set_b(block_granularity - 1, 0)
   }
   val conflict_mask = (0 until mshrs) map { i =>
     val s = io.mshr_status(i)
-    val s_conflict = s.valid && set_conflict(s.bits.set, input_set) && !s.bits.will_free
-    val a = io.mshr_alloc(i)
-    val a_conflict = a.valid && set_conflict(a.bits.set, input_set)
-    s_conflict | a_conflict
+    val s_conflict = s.valid && set_conflict(s.bits.set, in_set) && !s.bits.will_free
+    s_conflict
   }
   val conflict = Cat(conflict_mask).orR()
+  // filter out duplicated prefetch requests
+  val dup_mask = (0 until entries) map { i =>
+    valids(i) && (Cat(buffer(i).tag, buffer(i).set) === Cat(io.in.bits.tag, io.in.bits.set))
+  }
+  val dup = io.in.valid && io.in.bits.isPrefetch.getOrElse(false.B) && Cat(dup_mask).orR()
+  val req_deps = (0 until entries) map { i =>
+    valids(i) && set_conflict(buffer(i).set, in_set)
+  }
   val insert_idx = PriorityEncoder(~valids.asUInt())
-  when(!full && io.in.valid && !(flow.B && empty && io.out.ready)){
+  val alloc = !full && io.in.valid && !(flow.B && no_ready_entry && io.out.ready) && !dup
+  when(alloc){
     buffer(insert_idx) := io.in.bits
     valids(insert_idx) := true.B
     wait_table(insert_idx) := VecInit(conflict_mask).asUInt()
+    buffer_dep_mask(insert_idx) := VecInit(req_deps)
     assert(PopCount(conflict_mask) <= 1.U)
-    rdys(insert_idx) := !conflict
+    rdys(insert_idx) := !conflict && !Cat(req_deps).orR()
   }
 
-  val free_mask = VecInit(io.mshr_status.map(_.bits.will_free)).asUInt()
+  val free_mask = VecInit(io.mshr_status.map(s => s.valid && s.bits.will_free)).asUInt()
   for (i <- 0 until entries){
-    val wait_mask = wait_table(i).asUInt()
-    when((wait_mask & free_mask).orR()){
-      rdys(i) := true.B
+    when(issue_arb.io.out.fire()){
+      buffer_dep_mask(i)(issue_arb.io.chosen) := false.B
+    }
+    when(valids(i)){
+      wait_table(i) := wait_table(i).asUInt() & (~free_mask.asUInt()).asUInt()
+      rdys(i) := !wait_table(i).orR() && !Cat(buffer_dep_mask(i)).orR()
     }
   }
+
+  XSPerfAccumulate(cacheParams, "req_buffer_merge", dup && !full)
+  if(flow){
+    XSPerfAccumulate(cacheParams, "req_buffer_flow", no_ready_entry && io.in.fire())
+  }
+  XSPerfAccumulate(cacheParams, "req_buffer_alloc", alloc)
+  XSPerfAccumulate(cacheParams, "req_buffer_full", full)
+  for(i <- 0 until entries){
+    val update = PopCount(valids) === i.U
+    XSPerfAccumulate(cacheParams, s"req_buffer_util_$i", update)
+  }
+  XSPerfAccumulate(cacheParams, "recv_prefetch", io.in.fire() && io.in.bits.isPrefetch.getOrElse(false.B))
+  XSPerfAccumulate(cacheParams, "recv_normal", io.in.fire() && !io.in.bits.isPrefetch.getOrElse(false.B))
 
 }
--- src/main/scala/huancun/Slice.scala
+++ src/main/scala/huancun/Slice.scala
@@ -35,15 +35,13 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
   })
 
   // Inner channels
-  val a_req_buffer = Module(new RequestBuffer())
   val sinkA = Module(new SinkA)
   val sourceB = Module(new SourceB)
   val sinkC = Module(if (cacheParams.inclusive) new inclusive.SinkC else new noninclusive.SinkC)
   val sourceD = Module(new SourceD)
   val sinkE = Module(new SinkE)
 
-  a_req_buffer.io.in <> io.in.a
-  sinkA.io.a <> a_req_buffer.io.out
+  sinkA.io.a <> io.in.a
   io.in.b <> sourceB.io.b
   sinkC.io.c <> io.in.c
   io.in.d <> sourceD.io.d
@@ -92,6 +90,7 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
   dataStorage.io.sinkC_wdata <> sinkC.io.bs_wdata
 
   val mshrAlloc = Module(new MSHRAlloc)
+  val a_req_buffer = Module(new RequestBuffer())
   val probeHelperOpt = if(cacheParams.inclusive) None else Some(Module(new ProbeHelper))
 
   val a_req = Wire(DecoupledIO(new MSHRRequest()))
@@ -110,10 +109,11 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
     val alloc_A_arb = Module(new Arbiter(new MSHRRequest, 2))
     alloc_A_arb.io.in(0) <> a_req
     alloc_A_arb.io.in(1) <> io.prefetch.get.req
-    mshrAlloc.io.a_req <> alloc_A_arb.io.out
+    a_req_buffer.io.in <> alloc_A_arb.io.out
   } else {
-    mshrAlloc.io.a_req <> a_req
+    a_req_buffer.io.in <> a_req
   }
+  mshrAlloc.io.a_req <> a_req_buffer.io.out
   mshrAlloc.io.c_req <> sinkC.io.alloc
 
   ms.zipWithIndex.foreach {
@@ -130,7 +130,6 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
   abc_mshr.zipWithIndex.foreach{
     case (mshr, i) =>
       a_req_buffer.io.mshr_status(i) := mshr.io.status
-      a_req_buffer.io.mshr_alloc(i) := mshrAlloc.io.alloc(i)
   }
 
   val block_bc = c_mshr.io.status.valid
