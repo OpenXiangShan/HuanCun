4a5c2a6442fbc75d03cc412ea9b5886059b2d700
Yinan Xu <xuyinan@ict.ac.cn>
2022-05-24 21:22:34 +0800

    utils,sram: manually generate sram array

--- src/main/scala/huancun/utils/SRAMTemplate.scala
+++ src/main/scala/huancun/utils/SRAMTemplate.scala
@@ -25,7 +25,10 @@ import chisel3.util._
 import freechips.rocketchip.tilelink.LFSR64
 
 object HoldUnless {
-  def apply[T <: Data](x: T, en: Bool): T = Mux(en, x, RegEnable(x, 0.U.asTypeOf(x), en))
+  def apply[T <: Data](x: T, en: Bool, init: Option[T] = None): T = {
+    val hold_data = if (init.isDefined) RegEnable(x, init.get, en) else RegEnable(x, en)
+    Mux(en, x, hold_data)
+  }
 }
 
 object DelayTwoCycle {
@@ -40,6 +43,163 @@ object DelayTwoCycle {
 object ReadAndHold {
   def apply[T <: Data](x: Mem[T], addr:         UInt, en: Bool): T = HoldUnless(x.read(addr), en)
   def apply[T <: Data](x: SyncReadMem[T], addr: UInt, en: Bool): T = HoldUnless(x.read(addr, en), RegNext(en))
+}
+
+abstract class SRAM_Array extends RawModule {
+  def init(clock: Clock, writeClock: Option[Clock]): Unit
+  def read(addr: UInt): UInt
+  def read(addr: UInt, enable: Bool): UInt = {
+    var rdata = 0.U
+    when (enable) {
+      rdata = read(addr)
+    }
+    rdata
+  }
+  def write(addr: UInt, data: UInt): Unit
+  def write(addr: UInt, data: UInt, mask: UInt): Unit
+}
+
+class SRAM_Array_1P(depth: Int, width: Int, maskSegments: Int) extends SRAM_Array {
+  val RW0 = IO(new Bundle() {
+    val clk   = Input(Clock())
+    val addr  = Input(UInt(log2Ceil(depth).W))
+    val en    = Input(Bool())
+    val wmode = Input(Bool())
+    val wmask = if (maskSegments > 1) Some(Input(UInt(maskSegments.W))) else None
+    val wdata = Input(UInt(width.W))
+    val rdata = Output(UInt(width.W))
+  })
+
+  val ram = Seq.fill(maskSegments)(Mem(depth, UInt((width / maskSegments).W)))
+
+  withClock(RW0.clk) {
+    // read: rdata will keep stable until the next read enable.
+    val RW0_ren = RW0.en && !RW0.wmode
+    val RW0_ren_REG = RegNext(RW0_ren)
+    val RW0_addr_REG = RegEnable(RW0.addr, RW0_ren)
+    RW0.rdata := HoldUnless(VecInit(ram.map(_.read(RW0_addr_REG))).asUInt, RW0_ren_REG)
+    // write with mask
+    val RW0_wen = RW0.en && RW0.wmode
+    val wdata = RW0.wdata.asTypeOf(Vec(maskSegments, UInt((width / maskSegments).W)))
+    for (i <- 0 until maskSegments) {
+      val wmask = if (RW0.wmask.isDefined) RW0.wmask.get(i) else true.B
+      when (RW0_wen && wmask) {
+        ram(i)(RW0.addr) := wdata(i)
+      }
+    }
+  }
+
+  def init(clock: Clock, writeClock: Option[Clock] = None): Unit = {
+    dontTouch(RW0)
+    RW0 := DontCare
+    RW0.clk := clock
+    RW0.en := false.B
+  }
+  def read(addr: UInt): UInt = {
+    RW0.addr := addr
+    RW0.en := true.B
+    RW0.wmode := false.B
+    RW0.rdata
+  }
+  def write(addr: UInt, data: UInt): Unit = {
+    write(addr, data, ((1L << maskSegments) - 1).U)
+  }
+  def write(addr: UInt, data: UInt, mask: UInt): Unit = {
+    RW0.addr := addr
+    RW0.en := true.B
+    RW0.wmode := true.B
+    if (RW0.wmask.isDefined) {
+      RW0.wmask.get := mask
+    }
+    RW0.wdata := data
+  }
+}
+
+// MCP is used to distinguish SRAMs in Verilog. MCP SRAM is the same as normal SRAM.
+class SRAM_Array_1P_MCP(depth: Int, width: Int, maskSegments: Int) extends SRAM_Array_1P(depth, width, maskSegments)
+
+class SRAM_Array_2P(depth: Int, width: Int, maskSegments: Int) extends SRAM_Array {
+  require(width % maskSegments == 0)
+
+  val R0 = IO(new Bundle() {
+    val clk   = Input(Clock())
+    val addr  = Input(UInt(log2Ceil(depth).W))
+    val en    = Input(Bool())
+    val data  = Output(UInt(width.W))
+  })
+
+  val W0 = IO(new Bundle() {
+    val clk   = Input(Clock())
+    val addr  = Input(UInt(log2Ceil(depth).W))
+    val en    = Input(Bool())
+    val data  = Input(UInt(width.W))
+    val mask  = if (maskSegments > 1) Some(Input(UInt(maskSegments.W))) else None
+  })
+
+  val ram = Seq.fill(maskSegments)(Mem(depth, UInt((width / maskSegments).W)))
+
+  // read: rdata will keep stable until the next read enable.
+  withClock(R0.clk) {
+    val R0_ren = R0.en
+    val RW0_ren_REG = RegNext(R0_ren)
+    val RW0_addr_REG = RegEnable(R0.addr, R0_ren)
+    R0.data := HoldUnless(VecInit(ram.map(_.read(RW0_addr_REG))).asUInt, RW0_ren_REG)
+  }
+
+  // write with mask
+  withClock(W0.clk) {
+    val RW0_wen = W0.en
+    val wdata = W0.data.asTypeOf(Vec(maskSegments, UInt((width / maskSegments).W)))
+    for (i <- 0 until maskSegments) {
+      val wmask = if (W0.mask.isDefined) W0.mask.get(i) else true.B
+      when (RW0_wen && wmask) {
+        ram(i)(W0.addr) := wdata(i)
+      }
+    }
+  }
+
+  def init(clock: Clock, writeClock: Option[Clock]): Unit = {
+    dontTouch(R0)
+    dontTouch(W0)
+    R0 := DontCare
+    R0.clk := clock
+    R0.en := false.B
+    W0 := DontCare
+    W0.clk := writeClock.getOrElse(clock)
+    W0.en := false.B
+  }
+  def read(addr: UInt): UInt = {
+    R0.addr := addr
+    R0.en := true.B
+    R0.data
+  }
+  def write(addr: UInt, data: UInt): Unit = {
+    write(addr, data, ((1L << maskSegments) - 1).U)
+  }
+  def write(addr: UInt, data: UInt, mask: UInt): Unit = {
+    W0.addr := addr
+    W0.en := true.B
+    if (W0.mask.isDefined) {
+      W0.mask.get := mask
+    }
+    W0.data := data
+  }
+}
+
+object SRAM_Array {
+  def apply(clock: Clock, singlePort: Boolean, depth: Int, width: Int,
+            maskSegments: Int = 1,
+            MCP: Boolean = false,
+            writeClock: Option[Clock] = None
+           ): SRAM_Array = {
+    val array = (singlePort, MCP) match {
+      case (true, true) => Module(new SRAM_Array_1P_MCP(depth, width, maskSegments))
+      case (true, false) => Module(new SRAM_Array_1P(depth, width, maskSegments))
+      case (false, _) => Module(new SRAM_Array_2P(depth, width, maskSegments))
+    }
+    array.init(clock, writeClock)
+    array
+  }
 }
 
 class SRAMBundleA(val set: Int) extends Bundle {
@@ -110,12 +270,8 @@ class SRAMTemplate[T <: Data]
   })
 
   val wordType = UInt(gen.getWidth.W)
-  val array = SyncReadMem(set, Vec(way, wordType))
-  if(clk_div_by_2){
-    array.suggestName("sram_multi_cycle")
-  } else {
-    array.suggestName("sram")
-  }
+  val array = SRAM_Array(clock, singlePort, set, way * gen.getWidth, way, MCP = clk_div_by_2)
+
   val (resetState, resetSet) = (WireInit(false.B), WireInit(0.U))
 
   if (shouldReset) {
@@ -129,13 +285,12 @@ class SRAMTemplate[T <: Data]
 
   val (ren, wen) = (io.r.req.valid, io.w.req.valid || resetState)
   val realRen = (if (singlePort) ren && !wen else ren)
+  val raw_rdata = array.read(io.r.req.bits.setIdx, realRen).asTypeOf(Vec(way, wordType))
 
   val setIdx = Mux(resetState, resetSet, io.w.req.bits.setIdx)
   val wdata = VecInit(Mux(resetState, 0.U.asTypeOf(Vec(way, gen)), io.w.req.bits.data).map(_.asTypeOf(wordType)))
   val waymask = Mux(resetState, Fill(way, "b1".U), io.w.req.bits.waymask.getOrElse("b1".U))
-  when (wen) { array.write(setIdx, wdata, waymask.asBools) }
-
-  val raw_rdata = array.read(io.r.req.bits.setIdx, realRen)
+  when (wen) { array.write(setIdx, wdata.asUInt, waymask) }
 
   // bypass for dual-port SRAMs
   require(!bypassWrite || bypassWrite && !singlePort)
