2757fffe8b993b6b6d2b16dbdfd072d759cc002f
wangkaifan <wangkaifan@ict.ac.cn>
2022-07-13 17:11:02 +0800

    arb: latchedArb supports cancel operation based on mshr.io.enable
    * Note that tasks of sourceE should not depend on io.enable,
    * otherwise deadlock may occur

--- src/main/scala/huancun/utils/FastArbiter.scala
+++ src/main/scala/huancun/utils/FastArbiter.scala
@@ -50,6 +50,10 @@ class FastArbiter[T <: Data](gen: T, n: Int) extends FastArbiterBase[T](gen, n) 
 
 class LatchFastArbiter[T <: Data](gen: T, n: Int) extends FastArbiterBase[T](gen, n) {
 
+  val ctrl_io = IO(new Bundle {
+    val cancelVec = Input(Vec(n, Bool()))
+  })
+
   val out_valid_reg = RegInit(false.B)
   val out_bits_reg = RegInit(0.U.asTypeOf(io.out.bits))
   val chosen_reg = RegInit(0.U(n.W))
@@ -84,7 +88,8 @@ class LatchFastArbiter[T <: Data](gen: T, n: Int) extends FastArbiterBase[T](gen
     case (rdy, grant) => rdy := grant && out_valid_reg && io.out.ready
   }
 
-  io.out.valid := out_valid_reg && valids(OHToUInt(chosen_reg))
+  // io.out.valid := out_valid_reg && valids(OHToUInt(chosen_reg))
+  io.out.valid := out_valid_reg && !ctrl_io.cancelVec(OHToUInt(chosen_reg))
   io.out.bits <> out_bits_reg
   io.chosen := OHToUInt(chosen_reg)
 }
