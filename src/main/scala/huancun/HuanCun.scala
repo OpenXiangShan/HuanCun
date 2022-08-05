879709021711b5e52693ac114dc1461087c40850
wangkaifan <wangkaifan@ict.ac.cn>
2022-08-05 17:10:18 +0800

    misc: add one more SRAM lantency

--- src/main/scala/huancun/HuanCun.scala
+++ src/main/scala/huancun/HuanCun.scala
@@ -68,7 +68,7 @@ trait HasHuanCunParameters {
   // req -> sram ports 1 cycle
   // sram 1 or 2 cycles
   // sram ports -> channels 1 cycle
-  val sramLatency = 1 + 1 + (if(cacheParams.sramClkDivBy2) 2 else 1)
+  val sramLatency = 1 + 1 + (if(cacheParams.sramClkDivBy2) 3 else 1)
 
   val numCSRPCntHc    = 5
   val numPCntHcMSHR   = 7
