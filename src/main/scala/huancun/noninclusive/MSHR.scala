da7bc9d54db2a466182ed73862211a016bfc4105
wangkaifan <wangkaifan@ict.ac.cn>
2022-10-21 21:41:23 +0800

    mshr: do not send od.task until s_bypassput

--- src/main/scala/huancun/noninclusive/MSHR.scala
+++ src/main/scala/huancun/noninclusive/MSHR.scala
@@ -928,7 +928,7 @@ class MSHR()(implicit p: Parameters) extends BaseMSHR[DirResult, SelfDirWrite, S
   io.tasks.source_a.valid := io.enable && (!s_acquire || !s_transferput) && s_release && s_probe && can_start
   io.tasks.source_b.valid := io.enable && !s_probe && s_release
   io.tasks.source_c.valid := io.enable && (!s_release || !s_probeack && s_writerelease && w_probeack)
-  io.tasks.source_d.valid := io.enable && !s_execute && can_start && w_grant && s_writeprobe && w_probeacklast // TODO: is there dependency between s_writeprobe and w_probeack?
+  io.tasks.source_d.valid := io.enable && !s_execute && can_start && w_grant && s_writeprobe && w_probeacklast && s_transferput // TODO: is there dependency between s_writeprobe and w_probeack?
   io.tasks.source_e.valid := !s_grantack && w_grantfirst
   io.tasks.dir_write.valid := io.enable && !s_wbselfdir && no_wait && can_start
   io.tasks.tag_write.valid := io.enable && !s_wbselftag && no_wait && can_start
