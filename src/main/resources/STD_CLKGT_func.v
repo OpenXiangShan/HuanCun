1d3514235f1c20a0ff1a652a87cbe4cf317e959d
Yinan Xu <xuyinan@ict.ac.cn>
2022-11-02 14:13:50 +0800

    Update clock gating cell

--- src/main/resources/STD_CLKGT_func.v
+++ src/main/resources/STD_CLKGT_func.v
@@ -5,16 +5,29 @@ module STD_CLKGT_func (
   output wire Q
 );
 
+`ifdef SYNTHESIS
+  // Please include ICG cell here.
+`else
+
   wire clk_en;
   reg  clk_en_reg;
 
   assign clk_en = E | TE;
 
-  always @(posedge CK) 
-    begin
+`ifdef VERILATOR
+  always @(posedge CK) begin
+    clk_en_reg <= clk_en;
+  end
+`else
+  always_latch begin
+    if (!CK) begin
       clk_en_reg <= clk_en;
     end
+  end
+`endif // VERILATOR
 
   assign Q = CK & clk_en_reg;
 
-endmodule // Copy from Xihu
+`endif // SYNTHESIS
+
+endmodule
