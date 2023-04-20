47ce3f7d7ad3a8e580ab496bfe1538a0dae70a29
XiChen <chenxi171@mails.ucas.ac.cn>
2023-04-20 16:53:30 +0800

    TopDown: use PAddr from core to match MSHR misses

--- src/main/scala/huancun/TopDownMonitor.scala
+++ src/main/scala/huancun/TopDownMonitor.scala
@@ -3,14 +3,18 @@ package huancun
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 import chisel3.util._
-import huancun.utils.XSPerfHistogram
+import huancun.utils.{XSPerfAccumulate, XSPerfHistogram}
 
 class TopDownMonitor()(implicit p: Parameters) extends HuanCunModule {
   val banks = 1 << bankBits
   val io = IO(new Bundle() {
-    val cpuAddr  = Flipped(ValidIO(UInt(39.W))) // TODO: CPU giving PADDR of width ?
+    val cpuAddr  = Flipped(ValidIO(UInt(39.W))) // CPU using ExcitingUtils to give addr, this deprecated
     val msStatus = Vec(banks, Vec(mshrsAll, Flipped(ValidIO(new MSHRStatus))))
   })
+
+  val pAddr = Wire(Valid(UInt(36.W))) // TODO: hand written to match PAddrBits in SoC.scala
+  pAddr := DontCare
+  ExcitingUtils.addSink(pAddr, "rob_head_paddr", ExcitingUtils.Perf)
 
   /* ====== PART ONE ======
    * Check whether the Addr given by core is a Miss in Cache
@@ -19,13 +23,14 @@ class TopDownMonitor()(implicit p: Parameters) extends HuanCunModule {
     case(slice, i) =>
       slice.map {
         ms =>
-          val msAddr  = if(bankBits == 0) Cat(ms.bits.tag, ms.bits.set)
+          val msBlockAddr = if(bankBits == 0) Cat(ms.bits.tag, ms.bits.set)
                             else Cat(ms.bits.tag, ms.bits.set, i.U(bankBits-1, 0))
-          val cpuAddr = io.cpuAddr.bits & (~"h3F".U).asUInt
+//          val pLineAddr = io.cpuAddr.bits & (~"h3F".U).asUInt
+          val pBlockAddr  = pAddr.bits & (~"h3F".U).asUInt
 
           val isCPUReq = ms.valid && ms.bits.fromA && !ms.bits.is_prefetch // TODO: whether we need fromA
           val isMiss   = ms.bits.is_miss
-          msAddr === cpuAddr && isCPUReq && isMiss //TODO
+          pAddr.valid && msBlockAddr === pBlockAddr && isCPUReq && isMiss
       }
   }
 
@@ -35,6 +40,10 @@ class TopDownMonitor()(implicit p: Parameters) extends HuanCunModule {
 //    if(cacheParams.name == "L2") "IsL2Miss" else "IsL3Miss",
 //    ExcitingUtils.Perf)
 
+  XSPerfAccumulate(cacheParams,
+    if(cacheParams.name == "L2") "L2MissMatch" else "L3MissMatch",
+    addrMatch
+  )
 
   /* ====== PART TWO ======
    * Count the parallel misses, and divide them into CPU/Prefetch
