90aaf5935206ff322e461c3d021436c20dd0ac85
sinceforYy <1017657683@qq.com>
2025-01-03 13:10:56 +0800

    feat(TopDown): add l3Miss IO for Top-Down
    * Exclude all prefetch
    
    * Co-authored-by: Xi Chen <chenxi171@mails.ucas.ac.cn>

--- src/main/scala/huancun/Slice.scala
+++ src/main/scala/huancun/Slice.scala
@@ -38,6 +38,7 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
     val ctl_req = Flipped(DecoupledIO(new CtrlReq()))
     val ctl_resp = DecoupledIO(new CtrlResp())
     val ctl_ecc = DecoupledIO(new EccInfo())
+    val l3Miss = Output(Bool())
   })
   println(s"clientBits: $clientBits")
 
@@ -657,6 +658,12 @@ class Slice()(implicit p: Parameters) extends HuanCunModule {
       io.dir_result.get := directory.io.result
     }
   )
+  io.l3Miss := Cat(ms.init.init.map { m =>
+    m.io.status.valid && m.io.status.bits.channel(0) && (
+      m.io.status.bits.reqSource === MemReqSource.CPULoadData.id.U ||
+      m.io.status.bits.reqSource === MemReqSource.CPUStoreData.id.U
+    )
+  }).orR
 
   val perfinfo = IO(Output(Vec(numPCntHc, (UInt(6.W)))))
   perfinfo := DontCare
